bc9854e4659c50590b17785be8cf65e6980194b0
Elias Håkonsen <eliashaa@stud.ntnu.no>
2026-02-17 14:42:25 +0100

    Added new port

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,13 @@
-import {defineConfig} from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 80
-    }
+export default defineConfig(({ mode }) => {
+const env = loadEnv(mode, process.cwd(), '')
+
+return {
+plugins: [react()],
+server: {
+port: Number(env.VITE_PORT)
+}
+}
 })
--- server/main.py
+++ server/main.py
@@ -7,6 +7,7 @@ origins = [
     "http://localhost",
     "http://localhost:80",
     "http://localhost:81",
+     "http://localhost:5173",
     "http://127.0.0.1",
     "http://127.0.0.1:80",
     "http://127.0.0.1:81",
