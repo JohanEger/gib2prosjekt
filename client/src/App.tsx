f780a05fd6f8478722c40918ae72c560b950e9e4
Elias Håkonsen <eliashaa@stud.ntnu.no>
2026-02-23 12:29:05 +0100

    Removed response

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,46 +1,11 @@
 import "./App.css";
-import { useEffect, useState } from "react";
 import { Map } from "./components/Map";
 
 
-type HealthResponse = Record<string, string>; 
-
 function App() {
-  const [response, setResponse] = useState<HealthResponse>();
-
-  const getBackendApiStatus = async () => {
-    try {
-      const res = await fetch(
-        `${import.meta.env.VITE_BACKEND_BASE_URL}/health`,
-        {
-          headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-          },
-        },
-      );
-
-      if (!res.ok) {
-        throw new Error(`HTTP ${res.status}`);
-      }
-
-      const data: HealthResponse = await res.json();
-      setResponse(data);
-    } catch (err) {
-      setResponse({
-        error: err instanceof Error ? err.message : "Unknown error",
-      });
-    }
-  };
-
-  useEffect(() => {
-    getBackendApiStatus();
-  }, []);
-
-  return (
-    
-      <Map/>
   
+  return (
+    <Map/>
   );
 }
 
