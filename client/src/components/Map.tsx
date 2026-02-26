db5ce3aec9b7753c5f5e7a18f7d898b8f6a0d194
Elias Håkonsen <eliashaa@stud.ntnu.no>
2026-02-26 19:54:00 +0100

    Added color changing function and more dummy points

--- client/src/components/Map.tsx
+++ client/src/components/Map.tsx
@@ -1,7 +1,7 @@
 import { MapContainer, TileLayer, GeoJSON, Popup } from "react-leaflet";
-import { useState } from "react";
+import { useState, useRef, act } from "react";
 import type { FeatureCollection, Point } from "geojson";
-import L, { latLng, Layer, popup } from "leaflet";
+import L, { latLng, Layer, marker, popup } from "leaflet";
 
 export const Map = () => {
   const [data, setData] = useState<FeatureCollection<Point>>({
@@ -9,19 +9,40 @@ export const Map = () => {
     features: [
       {
         type: "Feature",
-        properties: { name: "Soundboks" },
+        id: "1",
+        properties: { name: "Soundboks 1" },
         geometry: {
           type: "Point",
           coordinates: [10.4, 63.43],
         },
       },
+      {
+        type: "Feature",
+        id: "2",
+        properties: { name: "Soundboks 2" },
+        geometry: {
+          type: "Point",
+          coordinates: [10.3969, 63.4269],
+        },
+      },
+      {
+        type: "Feature",
+        id: "3",
+        properties: { name: "Soundboks 3" },
+        geometry: {
+          type: "Point",
+          coordinates: [10.3927, 63.4225],
+        },
+      },
     ],
   });
 
-  const [selected, setSelected] = useState<{
+  const [hovered, setHovered] = useState<{
     feature: any;
     latLng: L.LatLng;
   } | null>(null);
+
+  const activeRef = useRef<L.CircleMarker | null>(null);
 
   return (
     <MapContainer
@@ -32,15 +53,15 @@ export const Map = () => {
       className="absolute  z-0"
     >
       <TileLayer
-        attribution="© OpenStreetMap contributors"
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="© OpenStreetMap contributors © Stadia Maps"
+        url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
       />
       <GeoJSON
         data={data}
         pointToLayer={(feature, latLng) =>
           L.circleMarker(latLng, {
             radius: 8,
-            fillColor: "#0b0070ff",
+            fillColor: "#acacacff",
             fillOpacity: 1,
             color: "#000",
             weight: 2,
@@ -49,25 +70,53 @@ export const Map = () => {
         onEachFeature={(feature, layer) => {
           layer.on({
             click: (e) => {
-              console.log("clicked");
-            },
-            mouseover: (e) => {
-              console.log("event latlng:", e.latlng);
+              const target = e.target as L.CircleMarker;
 
-              setSelected({
+              if (activeRef.current === target) {
+                target.setStyle({ fillColor: "#acacacff" });
+                activeRef.current = null;
+                return;
+              }
+
+              if (activeRef.current) {
+                activeRef.current.setStyle({ fillColor: "#acacacff" });
+              }
+
+              target.setStyle({ fillColor: "#0400ffff" });
+              activeRef.current = target;
+            },
+
+            mouseover: (e) => {
+              const target = e.target as L.CircleMarker;
+
+              if (activeRef.current !== target) {
+                target.setStyle({
+                  fillColor: "#0400ffff",
+                });
+              }
+
+              setHovered({
                 feature,
                 latLng: e.latlng,
               });
             },
+
             mouseout: (e) => {
-              setSelected(null);
+              const target = e.target as L.CircleMarker;
+
+              target.setStyle({
+                fillColor:
+                  activeRef.current === target ? "#0400ffff" : "#acacacff",
+              });
+
+              setHovered(null);
             },
           });
         }}
       ></GeoJSON>
-      {selected && (
-        <Popup position={selected.latLng} closeButton={false}>
-          {selected.feature.properties.name}
+      {hovered && (
+        <Popup position={hovered.latLng} closeButton={false}>
+          {hovered.feature.properties.name}
         </Popup>
       )}
     </MapContainer>
