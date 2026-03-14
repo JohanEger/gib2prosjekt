c875d8ad89ddfbc3423658e323e6ca11b7d7d14b
Elias Håkonsen <eliashaa@stud.ntnu.no>
2026-03-14 10:21:59 +0100

    changed import

--- server/app/database.py
+++ server/app/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy.ext import asyncio
+import asyncio
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine, async_sessionmaker
 import os
